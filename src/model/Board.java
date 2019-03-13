e5819ab30a8438dde43f4b1a2d1d3282d57864fa
Tanzim <s3642965@student.rmit.edu.au>
2019-03-13 19:10:31 +1100

    just checking if commit works

---
+++ src/model/Board.java
@@ -0,0 +1,12 @@
+package model;
+
+public class Board
+{
+    private int height;
+    private int breadth;
+    public Board(int height, int breadth)
+    {
+        this.height = height;
+        this.breadth = breadth;
+    }
+}
