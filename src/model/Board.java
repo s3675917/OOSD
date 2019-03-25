409534da88b7337fb3b6113579ee3c027af3a17f
rmit-s3642965-Tanzim-Shahriar <s3642965@student.rmit.edu.au>
2019-03-25 15:44:21 +1100

    added some classes to get started

--- src/model/Board.java
+++ src/model/Board.java
@@ -8,12 +8,21 @@ public class Board
     private int height;
     private int breadth;
 
-    private int board[][];
+    private int [][] board;
     private List<Pieces> pieces = new ArrayList<Pieces>();
+    public List<Pieces>getPieces()
+    {
+        return pieces;
+    }
+    public int [][] getBoard()
+    {
+        return board;
+    }
     public Board(int height, int breadth)
     {
         this.height = height;
         this.breadth = breadth;
+        this.board = new int[height][breadth];
         initBoard();
    }
 
@@ -30,25 +39,56 @@ public class Board
    }
 
    //Initializes all the pieces according to how player selects it from gui. User can rearrange the position of
-   // their 4 pieces however they want.
+   // their pieces however they want.
     public void initPieces()
     {
-        Pieces kingpin = new Kingpin(8,8);
-        Pieces key = new Key(3,3);
+        Prisoner kingpin = new Kingpin(0,6);
+        Pieces officer = new Officer(15,6);
+        Pieces dog = new Dog(15,7);
+        Pieces guard = new Guard(15,8);
+        Pieces key = new Key(8,7);
         pieces.add(kingpin);
+        pieces.add(officer);
+        pieces.add(dog);
+        pieces.add(guard);
         pieces.add(key);
+        updateBoard();
     }
 
-    //Prints out all the pieces and their positions
+
+    //sets the x,y of piece
+    public void setPiece(int toX, int toY, int pieceValue)
+    {
+        for(int k = 0; k < pieces.size(); k++)
+        {
+            if(pieceValue == pieces.get(k).getPieceValue())
+            {
+                board [pieces.get(k).getX()][pieces.get(k).getY()] = 0;
+                pieces.get(k).setPosition(toX,toY);}
+            }
+    }
+
+    //Prints out all the pieces and their positions, this is for testing only
     public void printBoard()
     {
-        for(int i = 0; i<pieces.size();i++)
-            System.out.println(pieces.get(i).toString());
+        for(int i = 0; i < height; i++)
+        {
+            for(int j = 0; j< breadth; j++)
+            {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 
-    //Updates the positions of pieces in board[][] array
+    //Updates the positions of pieces in board[][] array, 0 is empty, 1 is Kingpin, 2 is Prisoner, 3 is Officer,
+    //4 is dog, 5 is guard and 6 is key
     public void updateBoard()
     {
-
+        for(int i = 0; i < pieces.size(); i++)
+        {
+            board[pieces.get(i).getX()][pieces.get(i).getY()] = pieces.get(i).getPieceValue();
+        }
     }
 }
+
