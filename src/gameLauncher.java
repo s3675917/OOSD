5102261281476da9c8d71a754eb1bb249b528e67
rmit-s3642965-Tanzim-Shahriar <s3642965@student.rmit.edu.au>
2019-03-25 15:54:39 +1100

    merged with master

--- src/gameLauncher.java
+++ src/gameLauncher.java
@@ -2,9 +2,9 @@ import controller.SetupController;
 import view.SetupView;
 /*The game has 6 pieces, three pieces for each player/user. One player is going to play as prisoners and the other player
 is going to play as police officers. The officers have the pieces officer,dog,guard and prisoners have kingpin,.... . There is
-a key in the middle of the board that is moving left and right. The goal of the game is for one of the player/user to move the pieces
+a key in the middle of the board that is moving left and right. The goal of the game is for the players/users to move their assigned pieces
 and get the key to escape the prison or to protect the prison*/
-public class game {
+public class gameLauncher {
 public static void main(String[]args)throws Exception
         {
         SetupView setupView=new SetupView();
