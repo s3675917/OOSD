9d45768f985913d04ed9fc06c5c744137b9157cc
biao <s3675917@student.rmit.edu.au>
2019-04-03 14:37:36 +1100

    Apr 3 by Biao

---
+++ out/production/OOSD/res/tile/Wall.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.3" name="Wall" tilewidth="15" tileheight="15" spacing="1" margin="1" tilecount="1020" columns="20">
+ <image source="/Wall.png" width="320" height="816"/>
+</tileset>
